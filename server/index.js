8f7748f2fc98184bdb99cf2e6c9d295746874d10
Anurag236 <anuragkumar3260@gmail.com>
2024-09-12 23:53:56 +0530

    done

--- server/index.js
+++ server/index.js
@@ -48,10 +48,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
-
 
 
 app.use(cors({
